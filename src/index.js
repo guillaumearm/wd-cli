330bd507484f52e34125d0f7c494bcec0e8c2cda
Guillaume ARM <trapcodien@gmail.com>
2018-02-15 23:28:24 +0100

    style(eslint): fix errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-const R = require('ramda')
+// const R = require('ramda')
 const { fx, handler } = require ('./fx-handler')
 
+// eslint-disable-next-line no-console
 const log = fx(console.log)
 
 const test = handler(function* (...args) {
