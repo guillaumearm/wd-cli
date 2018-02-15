87bd45063f1411b2ff0d7860125d7d7d7233f9c8
Guillaume ARM <trapcodien@gmail.com>
2018-02-15 23:19:48 +0100

    feat(fx-handler): implement handler

--- src/fx-handler/index.js
+++ src/fx-handler/index.js
@@ -1,16 +1,35 @@
 const isEqual = require('lodash.isequal');
 
+const fxRunner = ({ f, args }) => f(...args)
+
 const createFx = (f, args) => {
   const fxObject = (...args) => createFx(f, args)
   fxObject.f = f;
   fxObject.args = args;
-  fxObject.run = (runner = (f, args) => f(...args)) => {
+  fxObject.run = (runner = fxRunner) => {
     if (!args) {
-      throw new Error('FX is not applied')
+      throw new Error('FX must be applied')
     }
-    return runner(f, args)
+    return runner(fxObject)
   }
   return fxObject
+}
+
+const createHandler = (fxGen, args) => {
+  const handlerObject = (...args) => createHandler(fxGen, args)
+  handlerObject.run = (runner = fxRunner) => {
+    if (!args) {
+      throw new Error('Handler must be applied')
+    }
+    const gen = fxGen(...args)
+    let genResult = gen.next()
+    while (!genResult.done) {
+      const { value: fx } = genResult
+      genResult = gen.next(fx.run(runner))
+    }
+    return genResult.value
+  }
+  return handlerObject
 }
 
 const fxEquals = (lfx, rfx) => {
@@ -18,6 +37,7 @@ const fxEquals = (lfx, rfx) => {
 }
 
 module.exports = {
+  handler: (fxGen) => createHandler(fxGen),
   fx: (f) => createFx(f, null),
   fxEquals,
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+
+const R = require('ramda')
+const { fx, handler } = require ('./fx-handler')
+
+const log = fx(console.log)
+
+const test = handler(function* (...args) {
+  for (const arg of args) {
+    yield log(arg)
+  }
+})
+
+test('a', 'b', 'c').run()
